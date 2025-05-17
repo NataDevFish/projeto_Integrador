6ee7f7314d1ac4f62c30cb79054e3c69042fa52d
Mariana <mariberteli@gmail.com>
2025-05-16 21:57:38 -0300

    Mariana

--- zooapp/urls.py
+++ zooapp/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("listar/", views.listar_itens, name="listar_itens"),
-    path("adicionar/", views.adicionar_item, name="adicionar_item"),
+    path("adicionar/", views.adicionar_item, name="adicionar_item")
 ]
